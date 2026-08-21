2803e3a2564eaacb8a98ebe177ff8b85e783a8c4
Tony <faracitonyp@gmail.com>
2026-08-21 14:58:10 +0200

    chore(materiel): purge legacy mon-materiel (suppression Phase 0) + gitignore fantomes

--- src/features/mon-materiel/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { AnimatedBackground } from './AnimatedBackground';
-export { AddToEquipmentButton } from './AddToEquipmentButton';
-export { CockpitFrame } from './CockpitFrame';
-export { GearCard } from './GearCard';
-export { MonMaterielGrid } from './MonMaterielGrid';
-export { FullscreenShell } from './FullscreenShell';
-export * from './icons';
